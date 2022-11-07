665b9220148a69ae33e330b8d4d206446162ca34
ckieverentineas <ckieverentineas@yandex.ru>
2022-11-07 11:45:09 +1000

    added base for pvp mechanics

--- src/engine/player.ts
+++ src/engine/player.ts
@@ -7,7 +7,7 @@ import { Player, Player_get } from "./core/user";
 import { Battle_Init } from './core/battle';
 import { prisma } from "..";
 
-
+const event: any = []
 export function registerUserRoutes(hearManager: HearManager<IQuestionMessageContext>): void {
 	hearManager.hear(/удалиться/, async (context) => {
         const player = await Player.build(context)
@@ -25,5 +25,47 @@ export function registerUserRoutes(hearManager: HearManager<IQuestionMessageCont
     })
     hearManager.hear(/битва/, async (context) => {
         await Battle_Init(context)
+    })
+    hearManager.hear(/pvp/, async (context) => {
+        const init_fight = await context.question(`
+                Готовы к пвп? Жмите Приступить!`,
+            {
+                keyboard: Keyboard.builder()
+                .textButton({   label: 'Начать',
+                                payload: {  command: 'pvp'  },
+                                color: 'secondary'              }).row()
+                .textButton({   label: 'Отмена',
+                                payload: {  command: 'back' },
+                                color: 'secondary'              }).oneTime()
+            }
+        )
+        const config: any = {    'pvp':  PVP_Init,
+                            'back': Back        }
+        config[init_fight.payload.command](context)
+        async function Back(context: any) {
+            
+        }
+        async function PVP_Init(context: any) {
+            let fight_end = false
+            while (fight_end == false) {
+                const turn = await context.question(`
+                        Events: ${event}`,
+                    {
+                        keyboard: Keyboard.builder()
+                        .textButton({   label: 'Атака',
+                                        payload: {  command: 'attack'   },
+                                        color: 'secondary'                  }).row()
+                        .textButton({   label: 'Отмена',
+                                        payload: {  command: 'back' },
+                                        color: 'secondary'
+                                                                            }).oneTime()
+                    }
+                )
+                if (turn) {
+                    event.push(`${context.senderId} did attack`)
+                }
+            }
+            
+        }
     })
 }
